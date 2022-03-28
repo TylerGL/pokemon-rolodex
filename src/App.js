10bd2a5808cabc0692468b9043b67f17d13b4dfc
TylerGL <tyler.gage.london@gmail.com>
2022-03-28 15:46:15 +0100

    Add SearchBox component and update CardList component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Component } from "react";
-import CardList from './components/card-list/card-list.component.jsx'
-
-function capFirstChar(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function getTypes(pokemon) {
-  const typelength = pokemon.types.length;
-  if (typelength === 2) {
-    // console.log(pokemon.types[0].type.name)
-    // console.log(pokemon.types[1].type.name)
-    return (
-      <p>
-        Types: {capFirstChar(pokemon.types[0].type.name)},{" "}
-        {capFirstChar(pokemon.types[1].type.name)}
-      </p>
-    );
-  } else if (typelength === 1) {
-    return <p>Type: {capFirstChar(pokemon.types[0].type.name)}</p>;
-  }
-}
+import CardList from "./components/card-list/card-list.component.jsx";
+import SearchBox from "./components/search-box/search-box.component.jsx";
 
 class App extends Component {
   constructor() {
@@ -33,7 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151") //full = 898
       .then((response) => response.json())
       .then((data) => {
         let results = data.results;
@@ -44,47 +25,32 @@ class App extends Component {
       })
       .then((data) =>
         this.setState({ pokemon: data }, () =>
-          // console.log("Main Pokemon State: ", this.state.pokemon)
+          console.log("Main Pokemon State: ", this.state.pokemon)
         )
       );
   }
 
-onSearchChange = (event) => {
-  const searchField = event.target.value.toLocaleLowerCase();
-  this.setState(() => {
-    return { searchField };
-  });
-}
-
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
 
   render() {
-    const {pokemon, searchField, } = this.state;
-    const {onSearchChange } = this;
+    const { pokemon, searchField } = this.state;
+    const { onSearchChange } = this;
     const filteredPokemon = pokemon.filter((pokemon) => {
       return pokemon.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="Search Pokemon"
-          onChange={onSearchChange}
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder="Search Pokémon"
+          className={'search-box'}
         />
-        {filteredPokemon.map((pokemon) => {
-          return (
-            <div key={pokemon.id}>
-              <h1>Name: {capFirstChar(pokemon.name)}</h1>
-              <p>ID: {pokemon.id}</p>
-              <p>Weight: {Math.round(pokemon.weight * 0.1 * 100) / 100}kg</p>
-              <p>Height: {Math.round(pokemon.height * 0.1 * 100) / 100}m</p>
-              <div>{getTypes(pokemon)}</div>
-              <img src={pokemon.sprites.front_default} alt="regular"></img>
-              <img src={pokemon.sprites.front_shiny} alt="shiny"></img>
-            </div>
-          );
-        })}
         <CardList pokemon={filteredPokemon} />
       </div>
     );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,11 +1,40 @@
 import { Component } from "react";
 
+function capFirstChar(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getTypes(pokemon) {
+  const typelength = pokemon.types.length;
+  if (typelength === 2) {
+    // console.log(pokemon.types[0].type.name)
+    // console.log(pokemon.types[1].type.name)
+    return (
+      <p>
+        Types: {capFirstChar(pokemon.types[0].type.name)},{" "}
+        {capFirstChar(pokemon.types[1].type.name)}
+      </p>
+    );
+  } else if (typelength === 1) {
+    return <p>Type: {capFirstChar(pokemon.types[0].type.name)}</p>;
+  }
+}
+
 class CardList extends Component {
   render() {
     const { pokemon } = this.props;
     console.log(this.props);
     console.log('render');
-    return pokemon.map((pokemon) => <div key={pokemon.id}> {pokemon.name} </div>);
+    return pokemon.map((pokemon) => 
+      <div key={pokemon.id}>
+        <h1>Name: {capFirstChar(pokemon.name)}</h1>
+        <p>ID: {pokemon.id}</p>
+        <p>Weight: {Math.round(pokemon.weight * 0.1 * 100) / 100}kg</p>
+        <p>Height: {Math.round(pokemon.height * 0.1 * 100) / 100}m</p>
+        <div>{getTypes(pokemon)}</div>
+        <img src={pokemon.sprites.front_default} alt="regular"></img>
+        <img src={pokemon.sprites.front_shiny} alt="shiny"></img>
+      </div>);
   }
 }
 
